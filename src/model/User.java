b189735c9c4910465f3212236e971dc163ab1769
leo <kuzmichleva1@gmail.com>
2025-10-16 21:10:18 +0200

    craete authentication

--- src/model/User.java
+++ src/model/User.java
@@ -1,36 +1,41 @@
 package model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class User {
     private String username;
     private String password;
+    private String dbFilePath = "src/data/users.txt";
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
     
     public boolean login() {
-        System.out.println("Logging in with " + this.username + " ...");
-        return true; // Simplified for 
-    }
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(dbFilePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(";");
 
-    public boolean logout() {
-        return true; // Simplified for 
+                String email = parts[0];
+                String password = parts[1];
+
+                if (email.equalsIgnoreCase(this.username) && password.equals(this.password)) {
+                    System.out.println("✅ Login successful for " + email);                    
+                    return true;
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error reading DB file: " + e.getMessage());
+        }
+
+        System.out.println("❌ Invalid username or password");
+        return false;
     }
 
     public boolean getUserInfo() {
